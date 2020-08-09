9a5e72dbf3a1a424f3fa3ba87fcdd65eed66b766
dubesoftware <zebra05@gmail.com>
2020-08-09 06:57:46 +0200

    Define microservice class with shell methods

---
+++ rpc_microservice.py
@@ -0,0 +1,25 @@
+from nameko.rpc import rpc
+
+
+class AcmeRPCMicroservice:
+    """A sample RPC microservice using the Nameko framework."""
+
+    name = "acme_rpc_microservice"
+
+    @rpc
+    def square_odd_numbers(self, items):
+        """Square each odd number in a given list of integers."""
+        return ""
+
+    @rpc
+    def huffman_encode_strings(self, items):
+        """Build a dictionary of strings - the key being the original string, 
+        and the value being a (Huffman) encoded version of that string.
+        """
+        return ""       
+
+    @rpc
+    def decode_huffman_encoded_string(self, item):
+        """Decode a given string previously Huffman encoded."""
+        return "" 
+
