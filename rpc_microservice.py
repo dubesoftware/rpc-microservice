1ad4e43430982c1f4f9937a5905dd7ec79bfca27
dubesoftware <zebra05@gmail.com>
2020-08-09 08:29:12 +0200

    Refactor decoding method parameter from list to string

--- rpc_microservice.py
+++ rpc_microservice.py
@@ -8,18 +8,29 @@ class AcmeRPCMicroservice:
 
     @rpc
     def square_odd_numbers(self, items):
-        """Square each odd number in a given list of integers."""
+        """Square each odd number in a given list of integers.
+
+        Keyword arguments:
+        items -- a list of integers
+        """
         return ""
 
     @rpc
     def huffman_encode_strings(self, items):
         """Build a dictionary of strings - the key being the original string, 
         and the value being a (Huffman) encoded version of that string.
+
+        Keyword arguments:
+        items -- a list of strings
         """
         return ""       
 
     @rpc
     def decode_huffman_encoded_string(self, item):
-        """Decode a given string previously Huffman encoded."""
+        """Decode a given string previously Huffman encoded.
+
+        Keyword arguments:
+        item -- a string
+        """
         return "" 
 
