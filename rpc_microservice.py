a3fe23374b6f02966ac09d7f7e6562b5e123c8d2
dubesoftware <zebra05@gmail.com>
2020-08-09 21:36:35 +0200

    Implement Huffman encoding of strings into key -> value pairs

--- rpc_microservice.py
+++ rpc_microservice.py
@@ -1,6 +1,9 @@
 from nameko.rpc import rpc
 
 
+from dahuffman import HuffmanCodec
+
+
 class AcmeRPCMicroservice:
     """A sample RPC microservice using the Nameko framework."""
 
@@ -30,7 +33,12 @@ class AcmeRPCMicroservice:
             A dictionary of Huffman encoded strings with original strings as 
             keys and encoded strings as values.
         """
-        return {}       
+        encoded_items = {}
+        for item in items:
+            item_codec = HuffmanCodec.from_data(item)
+            encoded_item = item_codec.encode(item)
+            encoded_items.update({item: encoded_item})
+        return encoded_items
 
     @rpc
     def decode_huffman_encoded_string(self, item):
